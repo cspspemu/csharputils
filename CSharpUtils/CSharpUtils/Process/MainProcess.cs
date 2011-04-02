a1d219ed2ed0dadd89ed392bc8b8fd4ea3435fa1
soywiz <soywiz@gmail.com>
2011-04-02 10:09:38 +0000

    - Process

---
+++ CSharpUtils/CSharpUtils/Process/MainProcess.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpUtils.Process
+{
+    class MainProcess : Process
+    {
+        public MainProcess()
+        {
+            currentExecutingProcess = this;
+        }
+
+        public bool Alive
+        {
+            get
+            {
+                return childs.Count > 0;
+            }
+        }
+
+        protected override void main()
+        {
+            while (Alive)
+            {
+                Yield();
+            }
+        }
+    }
+}
--- CSharpUtils/CSharpUtils/Process/Process.cs
+++ CSharpUtils/CSharpUtils/Process/Process.cs
@@ -8,135 +8,160 @@ using System.Runtime.InteropServices;
 
 namespace CSharpUtils.Process
 {
-	abstract class Process
-	{
-		[DllImport("kernel32.dll")]
-		extern static IntPtr ConvertThreadToFiber(int fiberData);
+    abstract class Process
+    {
+        [DllImport("kernel32.dll")]
+        extern static IntPtr ConvertThreadToFiber(int fiberData);
 
-		[DllImport("kernel32.dll")]
-		extern static IntPtr CreateFiber(int size, System.Delegate function, int handle);
+        [DllImport("kernel32.dll")]
+        extern static IntPtr CreateFiber(int size, System.Delegate function, int handle);
 
-		[DllImport("kernel32.dll")]
-		extern static IntPtr SwitchToFiber(IntPtr fiberAddress);
+        [DllImport("kernel32.dll")]
+        extern static IntPtr SwitchToFiber(IntPtr fiberAddress);
 
-		[DllImport("kernel32.dll")]
-		extern static void DeleteFiber(IntPtr fiberAddress);
+        [DllImport("kernel32.dll")]
+        extern static void DeleteFiber(IntPtr fiberAddress);
 
-		[DllImport("kernel32.dll")]
-		extern static int GetLastError();
+        [DllImport("kernel32.dll")]
+        extern static int GetLastError();
 
-		public enum State
-		{
-			Stopped,
-			Running,
-			Ended,
-		}
+        public enum State
+        {
+            Started,
+            Running,
+            Ended,
+        }
 
-		public State state;
-		static IntPtr mainFiberHandle = IntPtr.Zero;
-		IntPtr fiberHandle;
+        static protected Process currentExecutingProcess = null;
 
-		delegate void RunDelegate();
+        protected int priority = 0;
+        protected double x = 0, y = 0, z = 0;
+        protected Process parent = null;
+        protected LinkedList<Process> childs;
 
-		public Process()
-		{
-			state = State.Stopped;
-			if (mainFiberHandle == IntPtr.Zero)
-			{
-				mainFiberHandle = ConvertThreadToFiber(0);
-			}
-			fiberHandle = CreateFiber(500, new RunDelegate(() =>
-			{
-				state = State.Running;
-				main();
-				state = State.Ended;
-				Yield();
-			}), 0);
-		}
+        public State state { private set; get; }
+        private static IntPtr mainFiberHandle = IntPtr.Zero;
+        private IntPtr fiberHandle;
+        private delegate void RunDelegate();
 
-		~Process()
-		{
-			DeleteFiber(fiberHandle);
-		}
+        static private LinkedList<Process> _allProcesses = new LinkedList<Process>();
 
-		public void SwitchTo()
-		{
-			if (state != State.Ended)
-			{
-				SwitchToFiber(fiberHandle);
-			}
-		}
+        static public LinkedList<Process> allProcesses
+        {
+            get
+            {
+                return new LinkedList<Process>(_allProcesses);
+            }
+        }
 
-		protected void Yield()
-		{
-			SwitchToFiber(mainFiberHandle);
-		}
+        static public void _removeOld()
+        {
+            foreach (var process in allProcesses)
+            {
+                if (process.state == State.Ended) process._remove();
+            }
+        }
 
-		abstract protected void main();
-	}
+        protected void _ExecuteProcessBefore()
+        {
+            foreach (var process in childs.Where(process => process.priority < 0).OrderBy(process => process.priority)) process._ExecuteProcess();
+        }
 
-	/*
-	abstract class MyProcess : Process {
-		int n = 0;
+        protected void _ExecuteProcessAfter()
+        {
+            foreach (var process in childs.Where(process => process.priority >= 0).OrderBy(process => process.priority)) process._ExecuteProcess();
+        }
 
-		protected void increment()
-		{
-			while (n < +4)
-			{
-				n++;
-				Print();
-				Yield();
-			}
-		}
+        public void _ExecuteProcess()
+        {
+            if (state == State.Ended) return;
 
-		protected void decrement()
-		{
-			while (n > -4)
-			{
-				n--;
-				Print();
-				Yield();
-			}
-		}
+            currentExecutingProcess = this;
 
-		void Print()
-		{
-			Console.WriteLine(n);
-		}
+            this._ExecuteProcessBefore();
+            this.SwitchTo();
+            this._ExecuteProcessAfter();
+        }
 
-	}
+        protected void _DrawProcessBefore()
+        {
+            foreach (var process in childs.Where(process => process.z < 0).OrderBy(process => process.z)) process._DrawProcess();
+        }
 
-	class MyProcess1 : MyProcess
-	{
-		override protected void main()
-		{
-			increment();
-			decrement();
-		}
-	}
+        protected void _DrawProcessAfter()
+        {
+            foreach (var process in childs.Where(process => process.z >= 0).OrderBy(process => process.z)) process._DrawProcess();
+        }
 
-	class MyProcess2 : MyProcess
-	{
-		override protected void main()
-		{
-			decrement();
-			increment();
-		}
-	}
+        public void _DrawProcess()
+        {
+            this._DrawProcessBefore();
+            this.Draw();
+            this._DrawProcessAfter();
+        }
 
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			var f1 = new MyProcess1();
-			var f2 = new MyProcess2();
-			for (int n = 0; n < 20; n++)
-			{
-				f1.SwitchTo();
-				f2.SwitchTo();
-			}
-			Console.ReadKey();
-		}
-	}
-	*/
+        virtual protected void Draw()
+        {
+            //Console.WriteLine(this);
+        }
+
+        public Process()
+        {
+            _allProcesses.AddLast(this);
+            childs = new LinkedList<Process>();
+            parent = currentExecutingProcess;
+            if (parent != null)
+            {
+                parent.childs.AddLast(this);
+            }
+            //Console.WriteLine(this + " : " + parent);
+            state = State.Started;
+            if (mainFiberHandle == IntPtr.Zero)
+            {
+                mainFiberHandle = ConvertThreadToFiber(0);
+            }
+            fiberHandle = CreateFiber(500, new RunDelegate(() =>
+            {
+                state = State.Running;
+                main();
+                state = State.Ended;
+                Yield();
+            }), 0);
+        }
+
+        ~Process()
+        {
+            _remove();
+        }
+
+        private void _remove()
+        {
+            state = State.Ended;
+            if (parent != null)
+            {
+                parent.childs.Remove(this);
+            }
+            _allProcesses.Remove(this);
+            if (fiberHandle != IntPtr.Zero)
+            {
+                DeleteFiber(fiberHandle);
+                fiberHandle = IntPtr.Zero;
+            }
+        }
+
+        public void SwitchTo()
+        {
+            if (state != State.Ended)
+            {
+                SwitchToFiber(fiberHandle);
+            }
+        }
+
+        protected void Yield(int count = 1)
+        {
+            for (int n = 0; n < count; n++) SwitchToFiber(mainFiberHandle);
+        }
+
+        abstract protected void main();
+    }
 }
