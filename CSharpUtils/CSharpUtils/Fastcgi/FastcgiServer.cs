874a6b449b49f2e184636c95c0cbe8ea6f195c5e
soywiz <soywiz@gmail.com>
2011-10-07 12:11:03 +0200

    - Tabs for FastcgiServer class

--- CSharpUtils/CSharpUtils/Fastcgi/FastcgiServer.cs
+++ CSharpUtils/CSharpUtils/Fastcgi/FastcgiServer.cs
@@ -11,16 +11,16 @@ using System.Reflection;
 
 namespace CSharpUtils.Fastcgi
 {
-    public class FastcgiServer
-    {
-        ManualResetEvent AcceptMutex;
-        String Address;
-        ushort Port;
-        //TcpListener TcpListener;
+	public class FastcgiServer
+	{
+		ManualResetEvent AcceptMutex;
+		String Address;
+		ushort Port;
+		//TcpListener TcpListener;
 		Socket ServerSocket;
-        bool Async = false;
-        bool Debug = false;
-        //bool Debug = true;
+		bool Async = false;
+		bool Debug = false;
+		//bool Debug = true;
 
 		//[DllImport("Kernel32")]
 		//public static extern bool SetConsoleCtrlHandler(HandlerRoutine Handler, bool Add);
@@ -127,81 +127,81 @@ namespace CSharpUtils.Fastcgi
 			*/
 		}
 
-        /*
-        class ConnectionState
-        {
-            public AsyncCallback ReadAsyncCallback;
-            public Socket ClientSocket;
-            public const int DataSize = 1024;
-            public byte[] Data = new byte[DataSize];
-            public ProduceConsumeBuffer<byte> Buffer = new ProduceConsumeBuffer<byte>();
+		/*
+		class ConnectionState
+		{
+			public AsyncCallback ReadAsyncCallback;
+			public Socket ClientSocket;
+			public const int DataSize = 1024;
+			public byte[] Data = new byte[DataSize];
+			public ProduceConsumeBuffer<byte> Buffer = new ProduceConsumeBuffer<byte>();
 
-            public ConnectionState(Socket ClientSocket, AsyncCallback ReadAsyncCallback)
-            {
-                this.ClientSocket = ClientSocket;
-                this.ReadAsyncCallback = ReadAsyncCallback;
-            }
+			public ConnectionState(Socket ClientSocket, AsyncCallback ReadAsyncCallback)
+			{
+				this.ClientSocket = ClientSocket;
+				this.ReadAsyncCallback = ReadAsyncCallback;
+			}
 
-            public void BeginReceive()
-            {
-                ClientSocket.BeginReceive(Data, 0, DataSize, 0, ReadAsyncCallback, this);
-            }
-        }
+			public void BeginReceive()
+			{
+				ClientSocket.BeginReceive(Data, 0, DataSize, 0, ReadAsyncCallback, this);
+			}
+		}
 
-        void HandleAcceptedCallbackAsync(IAsyncResult AsyncResult) {
-            Socket ClientSocket = TcpListener.EndAcceptSocket(AsyncResult);
+		void HandleAcceptedCallbackAsync(IAsyncResult AsyncResult) {
+			Socket ClientSocket = TcpListener.EndAcceptSocket(AsyncResult);
 
-            AcceptMutex.Set();
+			AcceptMutex.Set();
 
-            var ConnectionState = new ConnectionState(ClientSocket, new AsyncCallback(HandleReadCallbackAsync));
-            ConnectionState.BeginReceive();
+			var ConnectionState = new ConnectionState(ClientSocket, new AsyncCallback(HandleReadCallbackAsync));
+			ConnectionState.BeginReceive();
 
-            Console.WriteLine("HandleAcceptedCallbackAsync");
-        }
+			Console.WriteLine("HandleAcceptedCallbackAsync");
+		}
 
-        void HandleReadCallbackAsync(IAsyncResult AsyncResult)
-        {
-            ConnectionState ConnectionState = (ConnectionState)AsyncResult.AsyncState;
-            Socket ClientSocket = ConnectionState.ClientSocket;
-            int ReadedCount = ClientSocket.EndReceive(AsyncResult);
+		void HandleReadCallbackAsync(IAsyncResult AsyncResult)
+		{
+			ConnectionState ConnectionState = (ConnectionState)AsyncResult.AsyncState;
+			Socket ClientSocket = ConnectionState.ClientSocket;
+			int ReadedCount = ClientSocket.EndReceive(AsyncResult);
 
-            Console.WriteLine("HandleReadCallbackAsync");
+			Console.WriteLine("HandleReadCallbackAsync");
 
-            if (ReadedCount > 0)
-            {
-                ConnectionState.Buffer.Produce(ConnectionState.Data, 0, ReadedCount);
-                Console.WriteLine(ConnectionState.Buffer.ConsumeRemaining);
-                ConnectionState.BeginReceive();
-            }
-            else
-            {
-                ClientSocket.Close();
-            }
-        }
-        */
+			if (ReadedCount > 0)
+			{
+				ConnectionState.Buffer.Produce(ConnectionState.Data, 0, ReadedCount);
+				Console.WriteLine(ConnectionState.Buffer.ConsumeRemaining);
+				ConnectionState.BeginReceive();
+			}
+			else
+			{
+				ClientSocket.Close();
+			}
+		}
+		*/
 
-        protected void HandleAcceptedSocket(Object _Socket)
-        {
-            var Socket = (Socket)_Socket;
-            if (Debug)
-            {
-                Console.WriteLine("HandleAcceptedSocket: " + Socket);
-            }
-            //Console.Write(".");
-            var FastcgiHandler = new FastcgiHandler(new FastcgiPipeSocket(Socket), Debug);
-            FastcgiHandler.HandleFastcgiRequest += HandleFascgiRequest;
-            FastcgiHandler.Reader.ReadAllPackets();
-        }
+		protected void HandleAcceptedSocket(Object _Socket)
+		{
+			var Socket = (Socket)_Socket;
+			if (Debug)
+			{
+				Console.WriteLine("HandleAcceptedSocket: " + Socket);
+			}
+			//Console.Write(".");
+			var FastcgiHandler = new FastcgiHandler(new FastcgiPipeSocket(Socket), Debug);
+			FastcgiHandler.HandleFastcgiRequest += HandleFascgiRequest;
+			FastcgiHandler.Reader.ReadAllPackets();
+		}
 
-        virtual protected void HandleFascgiRequest(FastcgiRequest FastcgiRequest)
-        {
-            using (var TextWriter = new StreamWriter(FastcgiRequest.StdoutStream))
-            {
-                TextWriter.WriteLine("X-Dynamic: C#");
-                TextWriter.WriteLine("Content-Type: text/html; charset=utf-8");
-                TextWriter.WriteLine("");
-                TextWriter.WriteLine("Not Implemented");
-            }
-        }
-    }
+		virtual protected void HandleFascgiRequest(FastcgiRequest FastcgiRequest)
+		{
+			using (var TextWriter = new StreamWriter(FastcgiRequest.StdoutStream))
+			{
+				TextWriter.WriteLine("X-Dynamic: C#");
+				TextWriter.WriteLine("Content-Type: text/html; charset=utf-8");
+				TextWriter.WriteLine("");
+				TextWriter.WriteLine("Not Implemented");
+			}
+		}
+	}
 }
