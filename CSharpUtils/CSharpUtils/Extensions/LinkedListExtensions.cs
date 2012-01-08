16ab506dd76d003c72e037c0b40bc2cfb15f5093
soywiz <soywiz@gmail.com>
2012-01-08 04:48:27 +0100

    - More Work

---
+++ CSharpUtils/CSharpUtils/Extensions/LinkedListExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpUtils.Extensions
+{
+	static public class LinkedListExtensions
+	{
+		static public T RemoveFirstAndGet<T>(this LinkedList<T> List)
+		{
+			try
+			{
+				return List.First.Value;
+			}
+			finally
+			{
+				List.RemoveFirst();
+			}
+		}
+
+		static public T RemoveLastAndGet<T>(this LinkedList<T> List)
+		{
+			try
+			{
+				return List.Last.Value;
+			}
+			finally
+			{
+				List.RemoveLast();
+			}
+		}
+	}
+}
