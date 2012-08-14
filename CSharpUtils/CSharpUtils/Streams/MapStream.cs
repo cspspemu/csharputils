cc8fde1f2a1d23013f6fde978d42c0d6b96d1960
soywiz <soywiz@gmail.com>
2012-08-15 00:03:00 +0200

    - Added Streams/MapStream and Streams/ZeroStream

---
+++ CSharpUtils/CSharpUtils/Streams/MapStream.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CSharpUtils.Streams
+{
+    public class MapStream : Stream
+    {
+        protected class StreamEntry
+        {
+            public long Position;
+            public Stream Stream;
+
+            public long Length { get { return Stream.Length; } }
+        }
+
+        protected List<StreamEntry> StreamEntries;
+        protected long _Position;
+        protected Stream _CurrentStream;
+        protected long _CurrentPositionInStream;
+
+        public MapStream()
+        {
+            StreamEntries = new List<StreamEntry>();
+        }
+
+        public MapStream Map(long Offset, Stream Stream)
+        {
+            StreamEntries.Add(new StreamEntry() { Position = Offset, Stream = Stream });
+            return this;
+        }
+
+        public override bool CanRead
+        {
+            get { return true; }
+        }
+
+        public override bool CanSeek
+        {
+            get { return true; }
+        }
+
+        public override bool CanWrite
+        {
+            get { return false; }
+        }
+
+        public override void Flush()
+        {
+            foreach (var StreamEntry in StreamEntries) StreamEntry.Stream.Flush();
+        }
+
+        public override long Length
+        {
+            get
+            {
+                return StreamEntries.Max(StreamEntry => StreamEntry.Position + StreamEntry.Length);
+            }
+        }
+
+        public override long Position
+        {
+            get
+            {
+                return _Position;
+            }
+            set
+            {
+                _Position = value;
+                _PrepareCurrentStream();
+            }
+        }
+
+        protected void _PrepareCurrentStream()
+        {
+            foreach (var StreamEntry in StreamEntries)
+            {
+                if (_Position >= StreamEntry.Position && _Position < StreamEntry.Position + StreamEntry.Length)
+                {
+                    _CurrentStream = StreamEntry.Stream;
+                    _CurrentPositionInStream = _Position - StreamEntry.Position;
+                    return;
+                }
+            }
+
+            _CurrentStream = null;
+            _CurrentPositionInStream = 0;
+
+            //_Position
+        }
+
+        protected long GetAvailableBytesOnCurrentStream()
+        {
+            return _CurrentStream.Length - _CurrentPositionInStream;
+        }
+
+        public override int Read(byte[] Buffer, int Offset, int Count)
+        {
+            if (_CurrentStream == null) throw (new InvalidOperationException(String.Format("Invalid/Unmapped MapStream position {0}", Position)));
+
+            var AvailableCount = (int)GetAvailableBytesOnCurrentStream();
+
+            if (Count > AvailableCount)
+            {
+                // Read from current Stream.
+                var Readed1 = Read(Buffer, Offset, AvailableCount);
+
+                if (Readed1 == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    // Try to read from the next Stream.
+                    var Readed2 = Read(Buffer, Offset + AvailableCount, Count - AvailableCount);
+
+                    return Readed1 + Readed2;
+                }
+            }
+            else
+            {
+                var ActualReaded = _CurrentStream.Read(Buffer, Offset, Count);
+                Position += ActualReaded;
+
+                return ActualReaded;
+            }
+        }
+
+        public override void Write(byte[] Buffer, int Offset, int Count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            switch (origin)
+            {
+                case SeekOrigin.Begin: Position = offset; break;
+                case SeekOrigin.Current: Position = Position + offset; break;
+                case SeekOrigin.End: Position = Length + offset; break;
+            }
+            return offset;
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CSharpUtils/CSharpUtilsTests/Streams/MapStreamTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using CSharpUtils.Streams;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpUtilsTests.Streams
+{
+    [TestClass]
+    public class MapStreamTest
+    {
+        [TestMethod]
+        public void TestRead()
+        {
+            var Stream1 = new ZeroStream(5, 0x11);
+            var Stream2 = new ZeroStream(3, 0x22);
+            var MapStream = new MapStream();
+            byte[] Readed1, Readed2, Readed3;
+
+            MapStream.Map(3, Stream1);
+            MapStream.Map(3 + 5, Stream2);
+
+            MapStream.Position = 4;
+
+            Readed1 = MapStream.ReadBytesUpTo(3);
+            CollectionAssert.AreEqual(new byte[] { 0x11, 0x11, 0x11 }, Readed1);
+
+            Readed2 = MapStream.ReadBytesUpTo(3);
+            CollectionAssert.AreEqual(new byte[] { 0x11, 0x22, 0x22 }, Readed2);
+
+            Readed3 = MapStream.ReadBytesUpTo(1);
+            CollectionAssert.AreEqual(new byte[] { 0x22 }, Readed3);
+        }
+    }
+}
