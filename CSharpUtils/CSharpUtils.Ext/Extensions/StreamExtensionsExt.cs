2e4d73cf6b83a1fcfa5adb6beff85126b96431b3
soywiz <soywiz@gmail.com>
2012-08-15 18:19:51 +0200

    - Test

--- CSharpUtils/CSharpUtils.Ext/Extensions/StreamExtensionsExt.cs
+++ CSharpUtils/CSharpUtils.Ext/Extensions/StreamExtensionsExt.cs
@@ -8,6 +8,41 @@ using CSharpUtils.Streams;
 
 static public class StreamExtensionsExt
 {
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <remarks>Presumes it is ordered.</remarks>
+	/// <param name="Spaces"></param>
+	/// <param name="Thresold"></param>
+	/// <returns></returns>
+	static public SpaceAssigner1D.Space[] JoinWithThresold(this SpaceAssigner1D.Space[] Spaces, int Thresold = 32)
+	{
+		var NewSpaces = new Stack<SpaceAssigner1D.Space>();
+		NewSpaces.Push(Spaces[0]);
+		for (int n = 1; n < Spaces.Length; n++)
+		{
+			var PrevSpace = NewSpaces.Pop();
+			var CurrentSpace = Spaces[n];
+
+			if (PrevSpace.Max - CurrentSpace.Min <= Thresold)
+			{
+				NewSpaces.Push(new SpaceAssigner1D.Space(PrevSpace.Min, CurrentSpace.Max));
+			}
+			else
+			{
+				NewSpaces.Push(PrevSpace);
+				NewSpaces.Push(CurrentSpace);
+			}
+		}
+		return NewSpaces.ToArray();
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="Stream"></param>
+	/// <param name="Spaces"></param>
+	/// <returns></returns>
 	static public MapStream ConvertSpacesToMapStream(this Stream Stream, SpaceAssigner1D.Space[] Spaces)
 	{
 		var MapStream = new MapStream();
--- CSharpUtils/CSharpUtils.Ext/Streams/ProxyStreamReadWriteAnalyzer.cs
+++ CSharpUtils/CSharpUtils.Ext/Streams/ProxyStreamReadWriteAnalyzer.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//#define DEBUG_ANALYZER
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,16 +9,33 @@ using CSharpUtils.SpaceAssigner;
 
 namespace CSharpUtils.Streams
 {
+	/// <summary>
+	/// 
+	/// </summary>
     public class ProxyStreamReadWriteAnalyzer : ProxyStream
     {
+		/// <summary>
+		/// 
+		/// </summary>
         protected SpaceAssigner1D _ReadUsage = new SpaceAssigner1D();
+
+		/// <summary>
+		/// 
+		/// </summary>
         protected SpaceAssigner1D _WriteUsage = new SpaceAssigner1D();
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="BaseStream"></param>
         public ProxyStreamReadWriteAnalyzer(Stream BaseStream)
             : base(BaseStream)
         {
         }
 
+		/// <summary>
+		/// 
+		/// </summary>
         public SpaceAssigner1D.Space[] ReadUsage
         {
             get
@@ -25,6 +44,9 @@ namespace CSharpUtils.Streams
             }
         }
 
+		/// <summary>
+		/// 
+		/// </summary>
         public SpaceAssigner1D.Space[] WriteUsage
         {
             get
@@ -33,12 +55,40 @@ namespace CSharpUtils.Streams
             }
         }
 
+		/// <summary>
+		/// 
+		/// </summary>
+		public override long Position
+		{
+			get
+			{
+				return base.Position;
+			}
+			set
+			{
+#if DEBUG_ANALYZER
+				//Console.WriteLine("Change position to {0}", value);
+#endif
+				base.Position = value;
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="buffer"></param>
+		/// <param name="offset"></param>
+		/// <param name="count"></param>
+		/// <returns></returns>
         public override int Read(byte[] buffer, int offset, int count)
         {
             var Start = Position;
 
             try
             {
+#if DEBUG_ANALYZER
+				Console.WriteLine("Read {0} bytes at {1}", count, Start);
+#endif
                 return base.Read(buffer, offset, count);
             }
             finally
@@ -47,13 +97,22 @@ namespace CSharpUtils.Streams
             }
         }
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="buffer"></param>
+		/// <param name="offset"></param>
+		/// <param name="count"></param>
         public override void Write(byte[] buffer, int offset, int count)
         {
             var Start = Position;
 
             try
             {
-                base.Write(buffer, offset, count);
+#if DEBUG_ANALYZER
+				Console.WriteLine("Write {0} bytes at {1}", count, Start);
+#endif
+				base.Write(buffer, offset, count);
             }
             finally
             {
