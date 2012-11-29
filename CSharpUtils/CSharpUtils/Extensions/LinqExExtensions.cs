a37c766c13e1ab15475286fccba7d9891f6ade64
soywiz <soywiz@gmail.com>
2012-11-29 23:55:43 +0100

    - Added a function

--- CSharpUtils/CSharpUtils/Extensions/LinqExExtensions.cs
+++ CSharpUtils/CSharpUtils/Extensions/LinqExExtensions.cs
@@ -89,6 +89,38 @@ public static class LinqExExtensions
 		return ListItems.ToArray();
 	}
 
+	public static T BinarySearch<T, TKey>(this IList<T> list, Func<T, TKey> keySelector, TKey key)
+			where TKey : IComparable<TKey>
+	{
+		int min = 0;
+		int max = list.Count;
+		while (min < max)
+		{
+			int mid = (max + min) / 2;
+			T midItem = list[mid];
+			TKey midKey = keySelector(midItem);
+			int comp = midKey.CompareTo(key);
+			if (comp < 0)
+			{
+				min = mid + 1;
+			}
+			else if (comp > 0)
+			{
+				max = mid - 1;
+			}
+			else
+			{
+				return midItem;
+			}
+		}
+		if (min == max &&
+			keySelector(list[min]).CompareTo(key) == 0)
+		{
+			return list[min];
+		}
+		throw new InvalidOperationException("Item not found");
+	}
+
 	/*
 	public static T ProcessNewObject<T>(T Object, Action<T> Callback)
 	{
